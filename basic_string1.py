6032064189ee2232b0e44f289f12a0f34c98fae4
Jimmy <8157881+weirdoj@users.noreply.github.com>
2021-10-19 17:57:19 +1300

    Create basic_string1.py

---
+++ basic_string1.py
@@ -0,0 +1,20 @@
+#Work with string
+type_of_people = 10
+x = f"There is {type_of_people} type of people"
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}"
+
+print (x)
+print (y)
+
+hilariou = False
+
+joke_evaluation = "Isn't that joke so funny"
+
+print(joke_evaluation.format(hilariou))
+
+w = "This is the left side of..."
+e = "a string with a right side"
+
+print(w + e)
